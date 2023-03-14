2f9994ea447f78edc33f97cd156d3c610ec52a9a
Todd Kamin <toddkamin@hotmail.com>
2023-03-14 12:14:05 -0600

    search input

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,42 +1,22 @@
-import {
-  View,
-  Text,
-  Image,
-  ImageBackground,
-  StyleSheet,
-  Button,
-} from "react-native";
+import { View, Text, Image, TextInput, StyleSheet, Button } from "react-native";
 import AccountScreen from "./AccountScreen";
 
 const HomeScreen = ({ navigation, route }) => {
-  /*
-  return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() => navigation.navigate("AccountScreen", { name: "Jane" })}
-    />
-  );
-  */
   return (
     <View style={[styles.container]}>
-      <Image
-        source={require("../assets/images/logo-text.png")}
-        style={{
-          flex: 1,
-          width: "90%",
-          height: 200,
-          resizeMode: "contain",
-          alignSelf: "center",
-        }}
-      />
-      <Text
-        style={{
-          flex: 1,
-          alignSelf: "center",
-        }}
-      >
-        Search bar goes here to start
-      </Text>
+      <View style={{ width: "100%" }}>
+        <Image
+          source={require("../assets/images/logo-text.png")}
+          style={{
+            width: "90%",
+            height: 250,
+            resizeMode: "contain",
+            alignSelf: "center",
+          }}
+        />
+        <View style={{ padding: 10 }}></View>
+        <TextInput style={styles.input} placeholder="Search for restaurants" />
+      </View>
     </View>
   );
 };
@@ -48,41 +28,17 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly",
     backgroundColor: "#fff",
   },
-  box: {
-    width: 50,
-    height: 50,
-  },
-  row: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-  },
-  button: {
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    backgroundColor: "oldlace",
-    alignSelf: "flex-start",
-    marginHorizontal: "1%",
-    marginBottom: 6,
-    minWidth: "48%",
-    textAlign: "center",
-  },
-  selected: {
-    backgroundColor: "coral",
-    borderWidth: 0,
-  },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: "500",
-    color: "coral",
-  },
-  selectedLabel: {
-    color: "white",
-  },
-  label: {
-    textAlign: "center",
-    marginBottom: 10,
-    fontSize: 24,
+  input: {
+    width: 300,
+    height: 40,
+    backgroundColor: "#fff",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 15,
+    fontSize: 16,
+    alignSelf: "center",
   },
 });
 export default HomeScreen;
