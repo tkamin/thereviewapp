49a0aaec046fd61d749637574c34bc7bd83e02b2
Todd Kamin <toddkamin@hotmail.com>
2023-03-17 17:46:38 -0600

    commonize the search bar component

---
+++ components/TRASearchBar.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { SearchBar } from "@rneui/themed";
+
+const TRASearchBar = ({ navigation, searchText }) => {
+  const [value, setValue] = React.useState(searchText);
+
+  return (
+    <SearchBar
+      platform="default"
+      containerStyle={{
+        backgroundColor: "#fff",
+        borderColor: "#ccc",
+        borderWidth: 1,
+        borderRadius: 15,
+        paddingVertical: 0,
+        paddingHorizontal: 10,
+        alignSelf: "center",
+        width: 300,
+      }}
+      inputContainerStyle={{ backgroundColor: "#fff" }}
+      inputStyle={{
+        borderColor: "#fff",
+        color: "#000",
+      }}
+      leftIconContainerStyle={{}}
+      rightIconContainerStyle={{}}
+      lightTheme
+      loadingProps={{}}
+      onChangeText={(newVal) => setValue(newVal)}
+      onClearText={() => console.log(`User cleared ${value}`)}
+      placeholder="Search for restaurants..."
+      placeholderTextColor="gray"
+      cancelButtonTitle="Cancel"
+      cancelButtonProps={{}}
+      onCancel={() => console.log(`User cancelled ${value}`)}
+      onSubmitEditing={() => {
+        navigation.navigate("SearchResults", { searchText: value });
+      }}
+      value={value}
+    />
+  );
+};
+
+export default TRASearchBar;
--- screens/SearchResultsScreen.jsx
+++ screens/SearchResultsScreen.jsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 import { View, Text, Image, TextInput, StyleSheet, Button } from "react-native";
-import AccountScreen from "./AccountScreen";
-import { ListItem, SearchBar } from "@rneui/themed";
+import { ListItem } from "@rneui/themed";
+import TRASearchBar from "../components/TRASearchBar";
 
 const SearchResultsScreen = ({ navigation, route }) => {
-  const [value, setValue] = React.useState(route.params.search);
-
   return (
     <View style={[styles.container]}>
       <View style={{ padding: 10 }}></View>
@@ -20,40 +18,10 @@ const SearchResultsScreen = ({ navigation, route }) => {
           }}
         />
         <View style={{ padding: 10 }}></View>
-        <SearchBar
-          platform="default"
-          containerStyle={{
-            backgroundColor: "#fff",
-            borderColor: "#ccc",
-            borderWidth: 1,
-            borderRadius: 15,
-            paddingVertical: 0,
-            paddingHorizontal: 10,
-            alignSelf: "center",
-            width: 300,
-          }}
-          inputContainerStyle={{ backgroundColor: "#fff" }}
-          inputStyle={{
-            borderColor: "#fff",
-            color: "#000",
-          }}
-          leftIconContainerStyle={{}}
-          rightIconContainerStyle={{}}
-          lightTheme
-          loadingProps={{}}
-          onChangeText={(newVal) => setValue(newVal)}
-          onClearText={() => console.log(`User cleared ${value}`)}
-          placeholder="Search for restaurants..."
-          placeholderTextColor="gray"
-          cancelButtonTitle="Cancel"
-          cancelButtonProps={{}}
-          onCancel={() => console.log(`User cancelled ${value}`)}
-          onSubmitEditing={() => {
-            navigation.navigate("SearchResults", { search: value });
-          }}
-          value={value}
+        <TRASearchBar
+          navigation={navigation}
+          searchText={route.params.searchText}
         />
-        {/*<TextInput style={styles.input} placeholder="Search for restaurants" />*/}
       </View>
     </View>
   );
